fa3525a87dd3e8ced87d78a9d5e05fe247f9b61a
nadimabdullah <48313623+nadimabdullah@users.noreply.github.com>
2022-06-14 13:20:28 +0600

    Add files via upload

---
+++ myfirstnodejs.js
@@ -0,0 +1,13 @@
+/*var http = require('http');
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content Type':'text/html'});
+    res.end('Hello World');
+
+}).listen(8080);*/
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
